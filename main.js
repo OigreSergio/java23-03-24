18af3e38f7ef6589eeb76a97ce9dd7c8bbeafda9
Gabriel <g.cellardoor.delloiacovo@gmail.com>
2024-02-05 16:20:38 +0100

    Create copy of myNumbers

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 function firstOne(numbers) {
   let newarray = [];
-  for (let i = 0; i < numbers.length; i++) {}
+  for (let i = 0; i < numbers.length; i++) {
+    let index = numbers[i];
+    newarray.push(index);
+  }
+  return newarray;
 }
+
+let myNumbers = [1, 2, 3, 4, 5];
+console.log(myNumbers);
+console.log(firstOne(myNumbers));
